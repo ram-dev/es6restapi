48dacd5171c676eef4c8cb2eaf23162df04c54ba
Ramkumar H <ramkumar.consult@gmail.com>
2016-11-08 14:11:44 +0530

    ES6 Rest API Boilerplate

---
+++ server/helpers/APIError.js
@@ -0,0 +1,90 @@
+import httpStatus from 'http-status';
+
+/**
+ * @extends Error
+ */
+class ExtendableError extends Error {
+  constructor(message, status, isPublic) {
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.status = status;
+    this.isPublic = isPublic;
+    this.isOperational = true; // This is required since bluebird 4 doesn't append it anymore.
+    Error.captureStackTrace(this, this.constructor.name);
+  }
+}
+
+/**
+ * Class representing an API error.
+ * @extends ExtendableError
+ */
+class APIError extends ExtendableError {
+  /**
+   * Creates an API error.
+   * @param {string} message - Error message.
+   * @param {number} status - HTTP status code of error.
+   * @param {boolean} isPublic - Whether the message should be visible to user or not.
+   */
+  constructor(message, status = httpStatus.INTERNAL_SERVER_ERROR, isPublic = false) {
+    super(message, status, isPublic);
+  }
+ 
+}
+
+function getErrorMessage(err){
+    var message = '';
+
+  if (err.code) {
+    switch (err.code) {
+      case 11000:
+      case 11001:
+        message = getUniqueErrorMessage(err);
+        break;
+      case 'LIMIT_FILE_SIZE':
+        message = 'Image too big. Please maximum ' + (100 / (1024 * 1024)).toFixed(2) + ' Mb files.';
+        break;
+      case 'LIMIT_UNEXPECTED_FILE':
+        message = 'Missing `newProfilePicture` field';
+        break;
+      default:
+        message = 'Something went wrong';
+    }
+  } else if (err.message && !err.errors) {
+    message = err.message;
+  } else {
+    for (var errName in err.errors) {
+      if (err.errors[errName].message) {
+        message = err.errors[errName].message;
+      }
+    }
+  }
+
+  return message;
+}
+
+function getUniqueErrorMessage(err){
+ var output;
+
+  try {
+    var begin = 0;
+    if (err.errmsg.lastIndexOf('.$') !== -1) {
+      // support mongodb <= 3.0 (default: MMapv1 engine)
+      // "errmsg" : "E11000 duplicate key error index: mean-dev.users.$email_1 dup key: { : \"test@user.com\" }"
+      begin = err.errmsg.lastIndexOf('.$') + 2;
+    } else {
+      // support mongodb >= 3.2 (default: WiredTiger engine)
+      // "errmsg" : "E11000 duplicate key error collection: mean-dev.users index: email_1 dup key: { : \"test@user.com\" }"
+      begin = err.errmsg.lastIndexOf('index: ') + 7;
+    }
+    var fieldName = err.errmsg.substring(begin, err.errmsg.lastIndexOf('_1'));
+    output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + ' already exists';
+
+  } catch (ex) {
+    output = 'Unique field already exists';
+  }
+
+  return output;
+}
+
+export default {APIError, getErrorMessage};
